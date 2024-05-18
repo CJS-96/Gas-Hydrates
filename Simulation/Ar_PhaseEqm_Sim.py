6b2f943e9a380da939d8e182c011713f21cfb9f7
CJS-96 <145349778+CJS-96@users.noreply.github.com>
2024-05-18 19:04:32 +0530

    Updated comments in Ar_PhaseEqm_Sim.py

--- Simulation/Ar_PhaseEqm_Sim.py
+++ Simulation/Ar_PhaseEqm_Sim.py
@@ -20,8 +20,8 @@ def SimFlFh_Ice(T,*ZHPdata):
     fiw0 = 3.219             ## fugacity of ice at ti0 and pi0
     Fico = np.array([1.8701e-05,4.4465e-09,-5.9784e-16,-4.28e-18,-68558,18.74])*(1/R)   ## Constants for Ice Equation state 
 
-    fhw0 = 31.2272           ## Fugacity of water in a real Ar-hydrate at ref temperature and pressure
-    Temperature = np.linspace(200,260,13)  ### Enthalpy temperature range
+    fhw0 = 31.2272           ## Fugacity of water in a filled Ar-hydrate at ref temperature and pressure
+    Temperature = np.linspace(200,260,13)  ### Hr temperature range
     lnfhw0 = math.log(fhw0)
     OnebyT = 1/Temperature
     poly = np.polyfit(OnebyT,HconstP,3)    ### Fitting H integrand with 1/T as variable
@@ -32,9 +32,9 @@ def SimFlFh_Ice(T,*ZHPdata):
     Hterm = poly[0]*(1/4)*((1/T)**4 - (1/t0)**4) + poly[1]*(1/3)*((1/T)**3 - (1/t0)**3)    \
             + poly[2]*(1/2)*((1/T)**2 - (1/t0)**2) + poly[3]*((1/T) - (1/t0))
 
-    lnfwhS = Hterm + Zterm + lnfhw0      ## log(fug of water in hydrate phase at T and Pressure(a))
+    lnfwhS = Hterm + Zterm + lnfhw0      ## log(fug of water in hydrate phase at T and input pressure)
 
-    #### Using fitted EOS for fugacity of water in liquid water ####
+    #### Using fitted EOS for fugacity of water in ice ####
     lnfwi = Fico[0]*(P/T - pi0/ti0) + Fico[1]*(P-pi0) + 0.5*Fico[2]*(P**2/T - pi0**2/ti0)   \
             + 0.5*Fico[3]*(P**2 - pi0**2) + Fico[4]*(1/T - 1/ti0) - Fico[5]*math.log(T/ti0) + math.log(fiw0)
 
@@ -50,10 +50,10 @@ def SimFlFh_Water(T,*ZHPdata):
     tw0 = 280;               ## ref temperature for liquid water
     pw0 = 20e5;              ## ref pressure for liquid water
     flw0 = 24.35;            ## fugacity of ice at tw0 and pw0
-    Flco = np.array([19.074e-06,-30.365e-10,-23.990e-15,56.020e-18,-74.170e03,58.817])*(1/R);
+    Flco = np.array([19.074e-06,-30.365e-10,-23.990e-15,56.020e-18,-74.170e03,58.817])*(1/R); ## Constants for Liquid water Equation state 
 
     fhw0 = 31.2272           ## Fugacity of water in a real Ar-hydrate at ref temperature and pressure
-    Temperature = np.linspace(260,310,51)  ### Enthalpy temperature range
+    Temperature = np.linspace(260,310,51)  ### Hr temperature range
 
     lnfhw0 = math.log(fhw0)
     OnebyT = 1/Temperature
@@ -71,9 +71,6 @@ def SimFlFh_Water(T,*ZHPdata):
             + 0.5*Flco[3]*(P**2 - pw0**2) + Flco[4]*(1/T - 1/tw0) - Flco[5]*math.log(T/tw0) + math.log(flw0)
 
     return lnfwhS - lnfwl   ## Function equating fug of water in liquid water and hyd phases, to be minimized.
-
-
-############# Main program to calculate I-H-V data ##################
 
 #### Reading Data for I-H-V
 def Read_IHV_data():
@@ -130,12 +127,12 @@ for a in range(0,(nd//npH)):
     HcombMat[a,:] = np.transpose(A)
 
 
-T0 = 245              ## Starting guess for finding T. Give a suitable initial guess, or even a range
+T0 = 245              ## Starting guess for finding T. Give a suitable initial guess, or even an array
 t0 = 280              ## Reference temperature of Hydrate
 
 #### Specify Pressures at which dissociation temperature is to be calculated ####
-Pressure = 1e6*np.array([0.8,1.6,2.4,3.2,4.0,5.0])               ## Pressure (Ar) at which the eqm T is calculated -- Ice-Hydrate
-#Pressure = 1e6*np.array([7.01,7.18,7.37,7.79,8.03,8.21,8.49])   ## Pressure (Ar) at which the eqm T is calculated -- Liquid Water-Hydrate
+Pressure = 1e6*np.array([0.8,1.6,2.4,3.2,4.0,5.0])               ## Pressure at which the eqm T is calculated -- Ice-Hydrate
+#Pressure = 1e6*np.array([7.01,7.18,7.37,7.79,8.03,8.21,8.49])   ## Pressure at which the eqm T is calculated -- Liquid Water-Hydrate
 npE = np.size(Pressure)           ## Number of Pressure points            
 TempSim = np.zeros((npE))         ## Dissociation temperature array initialized
 
@@ -144,9 +141,9 @@ ZcombMat=np.zeros((nz,npE))       ## The isotherm is integrated from reference p
 
 for a in range(0,npE):            ## Loop over the input pressure array
 #### Integrating Z term ####
-    xHgV = ETPVhyd[nz*a:nz*(a+1),2]      ## Taking the values needed to evaluate
-    xHwV = 1.0 - xHgV                    ## Z integrand at cont T=t0, upto different
-    P = ETPVhyd[nz*a:nz*(a+1),1]         ## P0 to Pressure(a).
+    xHgV = ETPVhyd[nz*a:nz*(a+1),2]      ## Taking the values needed to integrate
+    xHwV = 1.0 - xHgV                    ## isotherm at T=t0, upto different
+    P = ETPVhyd[nz*a:nz*(a+1),1]         ## Pressure(a).
     Vhyd = ETPVhyd[nz*a:nz*(a+1),4]
     HRgas, fug, Vgas = Argasprop(t0,P)   ## Calculating fugacity of Argon at t0 and P.
     Vgas = np.reshape(Vgas,-1)
@@ -179,8 +176,3 @@ for a in range(0,npE):            ## Loop over the input pressure array
 PhasePts = pd.DataFrame({'Pressure (Pa)' : Pressure , 'Temperature (K)' : TempSim}, columns=['Pressure (Pa)', 'Temperature (K)'])
 PhasePts.to_csv('TempSimI_Ar.dat', index=False, sep='\t')
 #PhasePts.to_csv('TempSimW_Ar.dat', index=False, sep='\t')
-
-
-
-
-
