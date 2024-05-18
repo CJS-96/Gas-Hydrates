2248125216a319f2a2156f27a86725618c6c77bb
CJS-96 <145349778+CJS-96@users.noreply.github.com>
2024-05-18 22:01:31 +0530

    Updated in comments Occupancy_Calculations.py

--- vdWP-FL_Theory/Occupancy_Calculations.py
+++ vdWP-FL_Theory/Occupancy_Calculations.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 from scipy.optimize import fsolve
 import math
 import pandas as pd
 
-
-# In[6]:
-
-
-lnfug = np.linspace(math.log(10),math.log(2.5e9),100)
+lnfug = np.linspace(math.log(10),math.log(2.5e9),100)       ## Range of fugacity to calculate occupancies.
 fug = np.exp(lnfug)
 len=np.size(fug)
 
-beta = 1/271.15
-focci = np.zeros((len,2))
-focc = np.zeros((len))
-fsmall1 = np.zeros((len))
-flarge1 = np.zeros((len))
-flarge2 = np.zeros((len))
-focci
+T=271.15
+beta = 1/T
+focci = np.zeros((len,2))       ## individual occupancy array
+focc = np.zeros((len))          ## Total occupancy array
+fsmall1 = np.zeros((len))       ## Contribution of first occupancy of small cage
+flarge1 = np.zeros((len))       ## Contribution of first occupancy of large cage
+flarge2 = np.zeros((len))       ## Contribution of second occupancy of large cage
 
 ### Gas Specific Info
 ### Argon:
-#Lc = np.array([5.8233249788031E-07,1.44062161093766E-06,5.80040718576832E-15])
-#aij = beta*np.array([[-100.128361799375, -36.4780370459466],[-76.7174404618716,-24.3812364686072]])
+#Lc = np.array([5.8233249788031E-07,1.44062161093766E-06,5.80040718576832E-15])                        ## Langmuir Constants
+#aij = beta*np.array([[-100.128361799375, -36.4780370459466],[-76.7174404618716,-24.3812364686072]])   ## Guest-Guest interactions
 
 ### Nitrogen
-Lc = np.array([1.66741366798993E-07,1.18368970284654E-06,5.87532363872445E-16])
-print(Lc.shape)
-aij = beta*np.array([[-105.864806350734, -38.5156603319702],[-80.7268703391869, -25.629540534039]])
+Lc = np.array([1.66741366798993E-07,1.18368970284654E-06,5.87532363872445E-16])                        ## Langmuir Constants
+aij = beta*np.array([[-105.864806350734, -38.5156603319702],[-80.7268703391869, -25.629540534039]])    ## Guest-Guest interactions
 
-for i in range(0,len):
-    thetan = np.array([0,1]);
+for i in range(0,len):                          ## Loop over fugacity points
+    thetan = np.array([0,1]);                   ## Initial guesses
 
-
-    ### options = optimoptions(@fsolve,'FunctionTolerance',1e-6);
-    def Occu(theta):
+    def Occu(theta):                            ## Function to calculate occupancy
         
         return theta - np.array([((math.exp(-(aij[0,0]*theta[0] + aij[0,1]*theta[1]))*fug[i]*Lc[0])/(1 + (math.exp(-(aij[0,0]*theta[0] + aij[0,1]*theta[1]))*fug[i]*Lc[0]))), \
                 (math.exp(-(aij[1,0]*theta[0] + aij[1,1]*theta[1]))*fug[i]*Lc[1] + (fug[i]**2)*Lc[2]*math.exp(-2*(aij[1,0]*theta[0] + aij[1,1]*theta[1])))/           \
@@ -57,19 +47,6 @@ for i in range(0,len):
 Occupancy = pd.DataFrame({'Fugacity (Pa)' : fug,'Total Occupancy' : focc, 'Small Cage Occupancy' : fsmall1, '1$^st$ Large Cage Occupancy' : flarge1, '2$^nd$ Large Cage Occupancy' : flarge2}, 
                           columns=['Fugacity (Pa)','Total Occupancy','Small Cage Occupancy','1$^st$ Large Cage Occupancy','2$^nd$ Large Cage Occupancy'])
 
-### Gas Specific name of the file
+#### Gas Specific name of the file ####
 #Occupancy.to_csv('Occupancy_Ar.dat', index=False, sep='\t')
 Occupancy.to_csv('Occupancy_N2.dat', index=False, sep='\t')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
